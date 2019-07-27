8335d7c0e99692d2839c7b9e1790d253ffd53d3e
Tristan Greaves <tristan@extricate.org>
2019-07-27 21:27:27 +0100

    Start of items and inventory management.  Generic.

---
+++ commands/drop.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Broadcast } = require('ranvier');
+const ArgParser = require('../lib/ArgParser');
+const ItemUtil = require('../lib/ItemUtil');
+
+module.exports = {
+  usage: 'drop <item>',
+  command : (state) => (args, player) => {
+    args = args.trim();
+
+    if (!args.length) {
+      return Broadcast.sayAt(player, 'Drop what?');
+    }
+
+    if (!player.room) {
+      return Broadcast.sayAt(player, 'You are floating in the nether, it would disappear forever.');
+    }
+
+    const item = ArgParser.parseDot(args, player.inventory);
+
+    if (!item) {
+      return Broadcast.sayAt(player, "You aren't carrying anything like that.");
+    }
+
+    player.removeItem(item);
+    player.room.addItem(item);
+    player.emit('drop', item);
+    item.emit('drop', player);
+
+    for (const npc of player.room.npcs) {
+      npc.emit('playerDropItem', player, item);
+    }
+
+    Broadcast.sayAt(player, `<green>You dropped: </green>${ItemUtil.display(item)}<green>.</green>`);
+  }
+};
