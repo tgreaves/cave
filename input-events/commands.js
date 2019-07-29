e1526e3bc077b740b14d5afb974ced6d79c4126c
Tristan Greaves <tristan@extricate.org>
2019-07-29 21:44:36 +0100

    Get boilerplate channels working.

--- input-events/commands.js
+++ input-events/commands.js
@@ -4,6 +4,10 @@ const { Broadcast: B, CommandType, Logger, PlayerRoles } = require('ranvier');
 const { NoPartyError, NoRecipientError, NoMessageError } = require('ranvier').Channel;
 const { CommandParser, InvalidCommandError, RestrictedCommandError } = require('../lib/CommandParser');
 
+/**
+ * Main command loop. All player input after login goes through here.
+ * If you want to swap out the command parser this is the place to do it
+ */
 module.exports = {
   event: state => player => {
     player.socket.once('data', data => {
@@ -16,12 +20,13 @@ module.exports = {
         return loop();
       }
 
+      player._lastCommandTime = Date.now();
+
       try {
         const result = CommandParser.parse(state, data, player);
         if (!result) {
           throw null;
         }
-
         switch (result.type) {
           case CommandType.MOVEMENT: {
             player.emit('move', result);
@@ -33,7 +38,48 @@ module.exports = {
             if (requiredRole > player.role) {
               throw new RestrictedCommandError();
             }
+            // commands have no lag and are not queued, just immediately execute them
             result.command.execute(result.args, player, result.originalCommand);
+            break;
+          }
+
+          case CommandType.CHANNEL: {
+            const { channel } = result;
+            if (channel.minRequiredRole !== null && channel.minRequiredRole > player.role) {
+              throw new RestrictedCommandError();
+            }
+            // same with channels
+            try {
+              channel.send(state, player, result.args);
+            } catch (error) {
+              switch (true) {
+                case error instanceof NoPartyError:
+                  B.sayAt(player, "You aren't in a group.");
+                  break;
+                case error instanceof NoRecipientError:
+                  B.sayAt(player, "Send the message to whom?");
+                  break;
+                case error instanceof NoMessageError:
+                  B.sayAt(player, `\r\nChannel: ${channel.name}`);
+                  B.sayAt(player, 'Syntax: ' + channel.getUsage());
+                  if (channel.description) {
+                    B.sayAt(player, channel.description);
+                  }
+                  break;
+              }
+            }
+            break;
+          }
+
+          case CommandType.SKILL: {
+            // See bundles/ranvier-player-events/player-events.js commandQueued and updateTick for when these
+            // actually get executed
+            player.queueCommand({
+              execute: _ => {
+                player.emit('useAbility', result.skill, result.args);
+              },
+              label: data,
+            }, result.skill.lag || state.Config.get('skillLag') || 1000);
             break;
           }
         }
@@ -63,4 +109,3 @@ module.exports = {
     });
   }
 };
-
--- player-events.js
+++ player-events.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { Broadcast: B } = require('ranvier');
+const sprintf = require('sprintf-js').sprintf;
+const LevelUtil = require('../cave/lib/LevelUtil');
+const { Broadcast: B, Config, Logger } = require('ranvier');
 
 module.exports = {
   listeners: {
@@ -13,6 +15,10 @@ module.exports = {
 
       if (!roomExit) {
         return B.sayAt(this, "You can't go that way!");
+      }
+
+      if (this.isInCombat()) {
+        return B.sayAt(this, 'You are in the middle of a fight!');
       }
 
       const nextRoom = state.RoomManager.getRoom(roomExit.roomId);
@@ -36,6 +42,19 @@ module.exports = {
 
       B.sayAt(oldRoom, `${this.name} leaves.`);
       B.sayAtExcept(nextRoom, `${this.name} enters.`, this);
+
+      for (const follower of this.followers) {
+        if (follower.room !== oldRoom) {
+          continue;
+        }
+
+        if (follower.isNpc) {
+          follower.moveTo(nextRoom);
+        } else {
+          B.sayAt(follower, `\r\nYou follow ${this.name} to ${nextRoom.title}.`);
+          follower.emit('move', movementCommand);
+        }
+      }
     },
 
     save: state => async function (callback) {
@@ -44,5 +63,61 @@ module.exports = {
         callback();
       }
     },
-  },
+
+    commandQueued: state => function (commandIndex) {
+      const command = this.commandQueue.queue[commandIndex];
+      const ttr = sprintf('%.1f', this.commandQueue.getTimeTilRun(commandIndex));
+      B.sayAt(this, `<bold><yellow>Executing</yellow> '<white>${command.label}</white>' <yellow>in</yellow> <white>${ttr}</white> <yellow>seconds.</yellow>`);
+    },
+
+    updateTick: state => function () {
+      if (this.commandQueue.hasPending && this.commandQueue.lagRemaining <= 0) {
+        B.sayAt(this);
+        this.commandQueue.execute();
+        B.prompt(this);
+      }
+      const lastCommandTime = this._lastCommandTime || Infinity;
+      const timeSinceLastCommand = Date.now() - lastCommandTime;
+      const maxIdleTime = (Math.abs(Config.get('maxIdleTime')) * 60000) || Infinity;
+
+      if (timeSinceLastCommand > maxIdleTime && !this.isInCombat()) {
+        this.save(() => {
+          B.sayAt(this, `You were kicked for being idle for more than ${maxIdleTime / 60000} minutes!`);
+          B.sayAtExcept(this.room, `${this.name} disappears.`, this);
+          Logger.log(`Kicked ${this.name} for being idle.`);
+          state.PlayerManager.removePlayer(this, true);
+        });
+      }
+    },
+
+    /**
+     * Handle player gaining experience
+     * @param {number} amount Exp gained
+     */
+    experience: state => function (amount) {
+      B.sayAt(this, `<blue>You gained <bold>${amount}</bold> experience!</blue>`);
+
+      const totalTnl = LevelUtil.expToLevel(this.level + 1);
+
+      // level up, currently wraps experience if they gain more than needed for multiple levels
+      if (this.experience + amount > totalTnl) {
+        B.sayAt(this, '                                   <bold><blue>!Level Up!</blue></bold>');
+        B.sayAt(this, B.progress(80, 100, "blue"));
+
+        let nextTnl = totalTnl;
+        while (this.experience + amount > nextTnl) {
+          amount = (this.experience + amount) - nextTnl;
+          this.level++;
+          this.experience = 0;
+          nextTnl = LevelUtil.expToLevel(this.level + 1);
+          B.sayAt(this, `<blue>You are now level <bold>${this.level}</bold>!</blue>`);
+          this.emit('level');
+        }
+      }
+
+      this.experience += amount;
+
+      this.save();
+    },
+  }
 };
